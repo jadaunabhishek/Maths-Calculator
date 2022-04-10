75ac58f641d4637fdd474936b2bb8046426c456a
Abhishek Jadaun <103330752+rajputasj@users.noreply.github.com>
2022-04-10 09:56:26 +0530

    Create calculator.c

---
+++ calculator.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+int a,b;
+printf("Enter value of a and b\n");
+scanf("%d %d\n",&a,&b);
+printf("Sum:%d\n",a+b);
+printf("Sub:%d\n",a-b);
+printf("Mul:%d\n",a*b);
+printf("Div:%d\n",a/b);
+return 0;
+}
